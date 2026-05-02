830434f6f50bbb96486ee13aa0f6e2e17e0976ba
Rahul Kushwaha <rahul7926963@gmail.com>
2026-05-02 18:53:36 +0545

    Add SplashScreen component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import TopNavbar from "@/components/layout/Navbar/TopNavbar";
 import Footer from "@/components/layout/Footer";
 import HolyLoader from "holy-loader";
 import Providers from "./providers";
+import SplashScreen from "@/SplashScreen"
 
 // --- RABINDRA STORE SEO & METADATA ---
 export const metadata: Metadata = {
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body className={`${satoshi.className} bg-slate-50 antialiased text-slate-900`}>
         <HolyLoader color="#16a34a" /> 
         
-        {/* Splash Screen Completely Removed */}
+        <SplashScreen />
 
         <Providers>
           <TopNavbar />
